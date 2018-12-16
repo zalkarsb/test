d9be7111840cd265881de79faf36651d1e04e447
szagvozdin123 <szagvozdin123@example.com>
2018-12-16 08:35:22 -0800

    Updated MySQL conf file to be able to route to the server from localhost\nMasked username/password of MySQL db on Application side

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ LABEL maintainer Sergei Z
 
 COPY php.ini /etc/php5/apache2/php.ini
 COPY dvwa /var/www/html
+COPY my.cnf /etc/mysql/my.cnf
 
 RUN chown www-data:www-data -R /var/www/html && \
     rm /var/www/html/index.html
--- dvwa/config/config.inc.php
+++ dvwa/config/config.inc.php
@@ -14,8 +14,8 @@ $DBMS = 'MySQL';
 $_DVWA = array();
 $_DVWA[ 'db_server' ]   = '127.0.0.1';
 $_DVWA[ 'db_database' ] = 'dvwa';
-$_DVWA[ 'db_user' ]     = 'root';
-$_DVWA[ 'db_password' ] = 'vulnerables';
+$_DVWA[ 'db_user' ]     = 'nobody';
+$_DVWA[ 'db_password' ] = 'changeme';
 
 # Only used with PostgreSQL/PGSQL database selection.
 $_DVWA[ 'db_port '] = '5432';
